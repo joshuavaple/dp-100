33cfe435f54a2cc2fda3111a4eef7ce986f1ce28
Ubuntu <phuonglevu3112@gmail.com>
2024-01-31 08:04:39 +0000

    added sample training script to log model artifacts within jobs (with/without sweep)

---
+++ diabetes/src/train_mlflow_log_model.py
@@ -0,0 +1,117 @@
+# import libraries
+import argparse
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
+import mlflow
+
+mlflow.autolog()
+
+# 1. The main "application" steps
+def main(args):
+    # read data
+    df = get_data(args.training_data)
+
+    # split data
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    # train model
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+    # evaluate model
+    eval_model(model, X_test, y_test)
+
+    # log model
+    mlflow.sklearn.log_model(sk_model=model,
+                             registered_model_name=args.registered_model_name,
+                             artifact_path=args.registered_model_name,
+                             )    
+
+# 2. The function to take user's arguments and parse them to be used later
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    # "dest" is not a must to have
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate',
+                        type=float, default=0.01)
+    parser.add_argument("--registered_model_name", 
+                        type=str, default="diabetes_classification_model", help="model name",)
+
+    # parse args and log them:
+    args = parser.parse_args()
+    mlflow.log_param("training_data", args.training_data)
+    mlflow.log_param("reg_rate", args.reg_rate)
+
+    # return args
+    return args
+
+# 3. The supporting function definitions that use the arguments from parse_args()
+# function that reads the data
+def get_data(path):
+    print("Reading data...")
+    df = pd.read_csv(path)
+    return df
+
+# function that splits the data
+def split_data(df):
+    print("Splitting data...")
+    X = df[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
+        'BMI', 'DiabetesPedigreeFunction', 'Age']].values
+    y = df['Outcome'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
+
+# function that trains the model
+def train_model(reg_rate, X_train, X_test, y_train, y_test):
+    print("Training model...")
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+
+    return model
+
+# function that evaluates the model
+def eval_model(model, X_test, y_test):
+    # calculate accuracy
+    y_hat = model.predict(X_test)
+    acc = np.average(y_hat == y_test)
+    print('Accuracy:', acc)
+
+    # calculate AUC
+    y_scores = model.predict_proba(X_test)
+    auc = roc_auc_score(y_test,y_scores[:,1])
+    print('AUC: ' + str(auc))
+
+    # plot ROC curve
+    fpr, tpr, thresholds = roc_curve(y_test, y_scores[:,1])
+    fig = plt.figure(figsize=(6, 4))
+    # Plot the diagonal 50% line
+    plt.plot([0, 1], [0, 1], 'k--')
+    # Plot the FPR and TPR achieved by our model
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+
+
+# 4. run script
+if __name__ == "__main__":
+    # add space in logs
+    print("\n\n")
+    print("*" * 60)
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+    
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
